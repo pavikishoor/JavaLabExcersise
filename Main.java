e395d713f77f5ea9b696f0a9ea23f137a1c87e62
pavikishoor <31266604+pavikishoor@users.noreply.github.com>
2018-09-29 15:07:48 +0530

    convertor_main

--- Main.java
+++ Main.java
@@ -1,11 +1,25 @@
+package com.Pavi.Converters;
 
+import java.util.Scanner;
+import com.Pavi.Main.*;
 public class Main {
 
-
 	public static void main(String[] args) {
-		 MyFrame f=new MyFrame();
-		
-
+    int Choice;
+    Scanner input=new Scanner(System.in);
+    Choice=input.nextInt();
+	switch(Choice){
+	case 1:
+		currency_convertor.EnterChoice();
+		break;
+	case 2:
+		DistanceConvertor.EnterChoice();
+		break;
+	case 3:
+		TimeConverters.EnterChoice();
+	default:
+		break;
 	}
-
+}
+	
 }
