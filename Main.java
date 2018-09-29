c15d581c98e2ad4dec6f79c96e2cb0c9574822a6
pavikishoor <31266604+pavikishoor@users.noreply.github.com>
2018-09-29 15:09:36 +0530

    Payroll

--- Main.java
+++ Main.java
@@ -1,24 +1,28 @@
+import java.io.*;
+import java.lang.*;
+class Employee{
+String Emp_name,Mail_id,Address;
+long Moblie_no;
+int Emp_id;
+int Bp;
+public Employee(int Bp)
+{
+	this.Bp=Bp;
+}
+void get_details(){
+	Scanner input = new Scanner(System.in);
+	System.out.println("Name :");
+	this.Emp_id=input.nextInt();
+}
 
-import java.io.File;
-import java.util.Scanner;
+
+
+
+
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Enter File Path With Name And Exit");
-		String fpath =new Scanner(System.in).nextLine();
-		File afile =new File(fpath);
-		
-		if(afile.exists()){
-			System.out.println("File is Available");
-		if(afile.canRead())
-			System.out.println("File Readable");
-		if(afile.canWrite())
-			System.out.println("File Writeable");
-		System.out.println(afile.getName());
-		System.out.println("File Size "+afile.length()+" Bytes ");
-	}else
-	{
-		System.out.println("FILE NOT fOUND");
+
 	}
-	}
+
 }
